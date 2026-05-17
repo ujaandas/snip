a76c30d6240190ebaaef946a80256f06b203274b
Ujaan Das <ujaandas03@gmail.com>
2026-05-17 20:50:45 +0000

    clean up old text and io pkgs

--- src/text/Buffer.cpp
+++
@@ -1,9 +0,0 @@
-#include "Buffer.h"
-
-Buffer::Buffer(QString text) : text_(text) {}
-
-QStringView Buffer::text() const { return text_; }
-
-qsizetype Buffer::scrollOffset() const { return scrollOffset_; }
-
-void Buffer::setScrollOffset(qsizetype offset) { scrollOffset_ = offset; }
