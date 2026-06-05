2c2525a9bb0ab2204eecbabc26260ef8e32e58ac
Ujaan Das <ujaandas03@gmail.com>
2026-06-05 23:05:10 +0100

    fix emit textChanged signal

--- src/editor/Editor.cpp
+++ src/editor/Editor.cpp
@@ -11,5 +11,10 @@ Editor::Editor(QObject* parent) : QObject(parent) {
   }
 }
 
-void Editor::setText(const QString& t) { m_text = t; }
 QString Editor::text() const { return m_text; }
+
+void Editor::setText(const QString& t) {
+  if (m_text == t) return;
+  m_text = t;
+  emit textChanged();
+}
