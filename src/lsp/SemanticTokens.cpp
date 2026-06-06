4a7328465ea2e6367466f37e4964947566d54c74
Ujaan Das <ujaandas03@gmail.com>
2026-06-06 21:52:22 +0100

    map tokens to colours

--- src/lsp/SemanticTokens.cpp
+++ src/lsp/SemanticTokens.cpp
@@ -1,6 +1,38 @@
 #include "SemanticTokens.hpp"
 
+#include <QColor>
+
 #include "Log.hpp"
+
+QString SemanticTokens::typeToString(int type) {
+  switch (type) {
+    case 0: return "type";
+    case 1: return "function";
+    case 2: return "variable";
+    case 3: return "parameter";
+    case 4: return "class";
+    case 5: return "enum";
+    case 6: return "interface";
+    case 7: return "struct";
+    case 8: return "typeParameter";
+    default: return "unknown";
+  }
+}
+
+QColor SemanticTokens::typeToColor(int type) {
+  switch (type) {
+    case 0: return QColor("#4EC9B0");   // type - teal
+    case 1: return QColor("#DCDCAA");   // function - yellow
+    case 2: return QColor("#9CDCFE");   // variable - light blue
+    case 3: return QColor("#9CDCFE");   // parameter - light blue
+    case 4: return QColor("#4EC9B0");   // class - teal
+    case 5: return QColor("#4EC9B0");   // enum - teal
+    case 6: return QColor("#4EC9B0");   // interface - teal
+    case 7: return QColor("#4EC9B0");   // struct - teal
+    case 8: return QColor("#4EC9B0");   // typeParameter - teal
+    default: return QColor("#D4D4D4");  // unknown - default text
+  }
+}
 
 QList<SemanticToken> SemanticTokens::parse(const QJsonArray& data) {
   QList<SemanticToken> tokens;
