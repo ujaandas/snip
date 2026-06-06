5c14ece2a91aa17572e8a19e94c43c2511068449
Ujaan Das <ujaandas03@gmail.com>
2026-06-06 21:52:34 +0100

    possible token ttypes

--- src/lsp/SemanticTokens.hpp
+++ src/lsp/SemanticTokens.hpp
@@ -11,7 +11,23 @@ struct SemanticToken {
   int modifiers;
 };
 
+// LSP token types (order matters - matches what we sent in capabilities)
+enum class TokenType {
+  Type = 0,
+  Function = 1,
+  Variable = 2,
+  Parameter = 3,
+  Class = 4,
+  Enum = 5,
+  Interface = 6,
+  Struct = 7,
+  TypeParameter = 8,
+  Unknown = 255
+};
+
 class SemanticTokens {
  public:
   static QList<SemanticToken> parse(const QJsonArray& data);
+  static QString typeToString(int type);
+  static QColor typeToColor(int type);
 };
