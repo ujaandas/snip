87ca93e14950d1e0017915af23051d2e6b9585c7
Ujaan Das <ujaandas03@gmail.com>
2026-06-07 06:36:01 +0100

    add more ts captures

--- src/tree-sitter/TreeSitter.cpp
+++ src/tree-sitter/TreeSitter.cpp
@@ -32,16 +32,134 @@ TreeSitter::~TreeSitter() {
 void TreeSitter::setupCaptureColors() {
   if (!theme_) return;
 
-  // use theme colors for syntax highlighting
+  // Core captures - Keywords
   captureColors_["keyword"] = theme_->hlKeyword;
-  captureColors_["operator"] = theme_->hlOperator;
-  captureColors_["string"] = theme_->hlString;
-  captureColors_["comment"] = theme_->hlComment;
-  captureColors_["number"] = theme_->hlNumber;
-  captureColors_["function"] = theme_->hlFunction;
+  captureColors_["keyword.function"] = theme_->hlKeyword;
+  captureColors_["keyword.operator"] = theme_->hlOperator;
+  captureColors_["keyword.return"] = theme_->hlKeyword;
+  captureColors_["keyword.import"] = theme_->hlKeyword;
+  captureColors_["keyword.export"] = theme_->hlKeyword;
+  captureColors_["keyword.conditional"] = theme_->hlKeyword;
+  captureColors_["keyword.repeat"] = theme_->hlKeyword;
+  captureColors_["keyword.exception"] = theme_->hlKeyword;
+  captureColors_["keyword.storage"] = theme_->hlKeyword;
+  captureColors_["keyword.directive"] = theme_->hlKeyword;
+  captureColors_["keyword.debug"] = theme_->hlKeyword;
+  captureColors_["conditional"] = theme_->hlKeyword;
+  captureColors_["repeat"] = theme_->hlKeyword;
+  captureColors_["include"] = theme_->hlKeyword;
+  captureColors_["exception"] = theme_->hlKeyword;
+  captureColors_["storageclass"] = theme_->hlKeyword;
+  captureColors_["label"] = theme_->hlKeyword;
+
+  // Types
   captureColors_["type"] = theme_->hlType;
+  captureColors_["type.builtin"] = theme_->hlType;
+  captureColors_["type.definition"] = theme_->hlType;
+  captureColors_["type.qualifier"] = theme_->hlKeyword;
+  captureColors_["type.parameter"] = theme_->hlType;
+  captureColors_["namespace"] = theme_->hlType;
+  captureColors_["class"] = theme_->hlType;
+  captureColors_["struct"] = theme_->hlType;
+  captureColors_["enum"] = theme_->hlType;
+  captureColors_["union"] = theme_->hlType;
+  captureColors_["interface"] = theme_->hlType;
+  captureColors_["typedef"] = theme_->hlType;
+
+  // Functions
+  captureColors_["function"] = theme_->hlFunction;
+  captureColors_["function.builtin"] = theme_->hlFunction;
+  captureColors_["function.call"] = theme_->hlFunction;
+  captureColors_["function.macro"] = theme_->hlFunction;
+  captureColors_["function.method"] = theme_->hlFunction;
+  captureColors_["function.method.call"] = theme_->hlFunction;
+  captureColors_["function.definition"] = theme_->hlFunction;
+  captureColors_["function.special"] = theme_->hlFunction;
+  captureColors_["macro"] = theme_->hlFunction;
+  captureColors_["method"] = theme_->hlFunction;
+  captureColors_["method.call"] = theme_->hlFunction;
+  captureColors_["constructor"] = theme_->hlFunction;
+  captureColors_["destructor"] = theme_->hlFunction;
+
+  // Variables
   captureColors_["variable"] = theme_->hlVariable;
+  captureColors_["variable.builtin"] = theme_->hlVariable;
+  captureColors_["variable.parameter"] = theme_->hlParameter;
+  captureColors_["variable.member"] = theme_->hlVariable;
+  captureColors_["parameter"] = theme_->hlParameter;
+  captureColors_["field"] = theme_->hlVariable;
+  captureColors_["property"] = theme_->hlVariable;
+  captureColors_["member"] = theme_->hlVariable;
+  captureColors_["constant"] = theme_->hlVariable;
+  captureColors_["constant.builtin"] = theme_->hlKeyword;
+  captureColors_["constant.macro"] = theme_->hlVariable;
+  captureColors_["self"] = theme_->hlKeyword;
+  captureColors_["this"] = theme_->hlKeyword;
+
+  // Literals
+  captureColors_["number"] = theme_->hlNumber;
+  captureColors_["number.float"] = theme_->hlNumber;
+  captureColors_["float"] = theme_->hlNumber;
+  captureColors_["string"] = theme_->hlString;
+  captureColors_["string.documentation"] = theme_->hlComment;
+  captureColors_["string.regex"] = theme_->hlString;
+  captureColors_["string.escape"] = theme_->hlKeyword;
+  captureColors_["string.special"] = theme_->hlString;
+  captureColors_["string.special.symbol"] = theme_->hlString;
+  captureColors_["string.special.url"] = theme_->hlString;
+  captureColors_["character"] = theme_->hlString;
+  captureColors_["character.special"] = theme_->hlKeyword;
+  captureColors_["boolean"] = theme_->hlKeyword;
+  captureColors_["null"] = theme_->hlKeyword;
+  captureColors_["undefined"] = theme_->hlKeyword;
+  captureColors_["literal"] = theme_->hlString;
+
+  // Comments
+  captureColors_["comment"] = theme_->hlComment;
+  captureColors_["comment.documentation"] = theme_->hlComment;
+  captureColors_["comment.error"] = theme_->hlError;
+  captureColors_["comment.warning"] = theme_->hlWarning;
+  captureColors_["comment.todo"] = theme_->hlTodo;
+  captureColors_["comment.note"] = theme_->hlInfo;
+
+  // Operators & Punctuation
+  captureColors_["operator"] = theme_->hlOperator;
   captureColors_["punctuation"] = theme_->hlPunctuation;
+  captureColors_["punctuation.delimiter"] = theme_->hlPunctuation;
+  captureColors_["punctuation.bracket"] = theme_->hlPunctuation;
+  captureColors_["punctuation.special"] = theme_->hlPunctuation;
+  captureColors_["punctuation.definition.string"] = theme_->hlString;
+  captureColors_["punctuation.definition.comment"] = theme_->hlComment;
+
+  // Tags (for HTML/XML/markup)
+  captureColors_["tag"] = theme_->hlTag;
+  captureColors_["tag.builtin"] = theme_->hlTag;
+  captureColors_["tag.delimiter"] = theme_->hlPunctuation;
+  captureColors_["attribute"] = theme_->hlAttribute;
+  captureColors_["attribute.builtin"] = theme_->hlAttribute;
+
+  // Preprocessor/Directives
+  captureColors_["preproc"] = theme_->hlKeyword;
+  captureColors_["define"] = theme_->hlKeyword;
+  captureColors_["preprocessor"] = theme_->hlKeyword;
+  captureColors_["directive"] = theme_->hlKeyword;
+
+  // Special
+  captureColors_["module"] = theme_->hlModule;
+  captureColors_["import"] = theme_->hlModule;
+  captureColors_["export"] = theme_->hlModule;
+  captureColors_["escape"] = theme_->hlKeyword;
+  captureColors_["format"] = theme_->hlKeyword;
+  captureColors_["repeat.intro"] = theme_->hlKeyword;
+
+  // Diff
+  captureColors_["diff.plus"] = theme_->hlDiffAdd;
+  captureColors_["diff.minus"] = theme_->hlDiffDelete;
+  captureColors_["diff.delta"] = theme_->hlDiffChange;
+
+  // Conceal/Noise
+  captureColors_["conceal"] = theme_->textMuted;
+  captureColors_["noise"] = theme_->textMuted;
 }
 
 QString TreeSitter::readQueryFile(const QString &queryDir,
